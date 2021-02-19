73406681217e5083a732b0ad808a057a0390903e
Leoff00 <leof0180@gmail.com>
2021-02-19 20:43:19 -0300

    changing the path file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-app.use(express.static(__dirname + '/static/'))
+app.use(express.static(__dirname + '/public/'))
 
 app.listen(3333, () => {
     console.log('Listening on PORT: 3333');
